090a90fc3295348782a285c0f494ee26ae502e4d
Mert Arda Asar <ardaasar0@gmail.com>
2024-01-09 11:41:00 +0300

    graph building process output beautified

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def bot_response(message, history):
     global file
 
     if "generate from file:" in message:
+        queries_str = ""
         unique_entity_type_list, unique_relation_list = [], []
         entity_list, type_list, relationship_list, entity_type_list = [], [], [], []
 
@@ -64,7 +65,7 @@ def bot_response(message, history):
         print("\nEntities:", entity_type_list)
 
         yield "Given file is loaded"
-        time.sleep(1)
+        time.sleep(2)
         yield f"Entities:{entity_type_list}\nRelationships{relationship_list}"
 
         for id, entity_type in enumerate(entity_type_list):
@@ -80,14 +81,17 @@ def bot_response(message, history):
 
             relationship_list[id] = [source.strip().replace(" ", "_"), relationship.strip().replace(" ", "_"), target.strip().replace(" ", "_")] 
 
-        time.sleep(3)
+        time.sleep(2)
         generated_queries = cypher_query(relationship_list, entity_type_list)
-        yield f"Generated Queries: {generated_queries}"
-
+        for q in generated_queries:
+            queries_str = f"\n{q}"
+        
         for q in generated_queries: 
             execute_query(q, graph)        
+        queries_str += "\nDone"
 
-        yield "Graph created successfully"
+        yield queries_str
+
     elif "run query:" in message:
         q = message.split("run query:")[1].strip()
         resp = execute_query(q, graph)
